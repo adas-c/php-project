3a62cb59fbce8e6a421513f3f74f64edc60f9119
Adam Ciołek <adasciolek96@gmail.com>
2023-07-31 19:18:48 +0200

    add single-event.php, archive-event.php

--- functions.php
+++ functions.php
@@ -23,6 +23,10 @@ add_action('after_setup_theme', 'university_features');
 
 // function uni_post_types() {
 //   register_post_type('event', array(
+//     'rewrite' => array(
+//       'slug' => 'events'
+//     ),
+//     'has_archive' => true,
 //     'public' => true,
 //     'show_in_rest' => true,
 //     'labels' => array(
