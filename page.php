cf74fbd25eaeefba4805c4e6f471fddd6a63df9e
Adam Ciołek <adasciolek96@gmail.com>
2023-07-31 14:32:08 +0200

    menu of child page link

--- page.php
+++ page.php
@@ -26,15 +26,31 @@
         ?>
     </div>
     
-    <!--
-    <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
-      <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
-      </ul>
-    </div>
-    -->
+    <?php 
+    $testArray = get_pages(array(
+      'child_of' => get_the_ID()
+    ));
+
+    if ($linkParent || $testArray) { ?>
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($linkParent); ?>"><?php echo get_the_title($linkParent); ?></a></h2>
+        <ul class="min-list">
+          <?php 
+            if ($linkParent) {
+              $findChildrenOf = $linkParent;
+            } else {
+              $findChildrenOf = get_the_ID();
+            };
+  
+            wp_list_pages(array(
+              'title_li' => NULL,
+              'child_of' => $findChildrenOf
+            ));
+          ?>
+        </ul>
+      </div>
+    <?php } ?>
+   
 
     <div class="generic-content">
       <?php the_content(); ?>
