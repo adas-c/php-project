1f14c463e3387a54decede7e9d04f1e24cd7f9a5
Adam Ciołek <adasciolek96@gmail.com>
2023-08-04 11:14:34 +0200

    add multiple post type wp_query

--- inc/search-route.php
+++ inc/search-route.php
@@ -7,8 +7,61 @@ function uniRegisterSearch() {
   ));
 }
 
-function uniSearchResults() {
-  return 'con';
+function uniSearchResults($data) {
+  $mainQuery = new WP_Query(array(
+    'post_type' => array('post','page','professor','program','event','campuses'),
+    's' => sanitize_text_field($data['term'])
+  ));
+
+  $results = array(
+    'generalInfo' => array(),
+    'professors' => array(),
+    'programs' => array(),
+    'events' => array(),
+    'campuses' => array()
+  );
+
+  while($mainQuery->have_posts()) {
+    $mainQuery->the_post();
+
+    if (get_post_type() == 'post' || get_post_type() == 'page') {
+      array_push($results['generalInfo'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
+
+    if (get_post_type() == 'professors') {
+      array_push($results['professors'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
+
+    if (get_post_type() == 'events') {
+      array_push($results['events'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+
+    if (get_post_type() == 'programs') {
+      array_push($results['programs'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
+
+    if (get_post_type() == 'campuses') {
+      array_push($results['campuses'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
+  }
+
+  }
+
+  return $results;
 }
 
 add_action('rest_api_init', 'uniRegisterSearch');
