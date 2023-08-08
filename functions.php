3fbc61922ab61219566afd88600d67032445c704
Adam Ciołek <adasciolek96@gmail.com>
2023-08-08 17:05:25 +0200

    user roles and permissions

--- functions.php
+++ functions.php
@@ -96,12 +96,59 @@ function uni_adjust_queries($query) {
 add_action('pre_get_posts', 'uni_adjust_queries');
 
 
+// redirect sub accounts out of admin and onto homepage
+add_action('admin_init', 'redirectSubsToFrontend');
+
+function redirectSubsToFrontend() {
+  $ourCurrentUser = wp_get_current_user();
+
+  if (count($ourCurrentUser->roles) == 1 && $ourCurrentUser->roles[0] == 'subscriber') {
+    wp_redirect(site_url('/'));
+    exit;
+  }
+}
+
+add_action('wp_loaded', 'noSubAdminBar');
+
+function noSubAdminBar() {
+  $ourCurrentUser = wp_get_current_user();
+
+  if (count($ourCurrentUser->roles) == 1 && $ourCurrentUser->roles[0] == 'subscriber') {
+    show_admin_bar(false);
+  }
+}
+
+//  Customize login Screen
+
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function ourHeaderUrl() {
+  return esc_url(site_url('/'));
+}
+
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+function ourLoginCSS() {
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+  wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+}
+
+
+add_filter('login_headertitle', 'ourHeaderTitle');
+
+function ourHeaderTitle(){
+  return get_bloginfo('name');
+}
 
 // function moved to new file called 'must use plugins'
 
 // function uni_post_types() {
 //   // Events post type
 //   register_post_type('event', array(
+//     'capability_type' => 'event',
+//     'map_meta_cap' => true,
 //     'supports' => array('title','editor','excerpt'),
 //     'rewrite' => array(
 //       'slug' => 'events'
